7f9fa809ccf82efef32561eefa58d284695d03eb
nouranraouf <nouran.raouf@gmail.com>
2021-10-21 19:30:57 +0200

    Update and rename my_first_assignment.py to monster-game.py

---
+++ monster-game.py
@@ -0,0 +1,12 @@
+###############################
+##                            #
+## MY FIRST PYTHON ASSIGNMENT #
+##                            #
+##         \o/                #
+###############################
+
+# YOUR ASSIGNMENT:
+# 1. add below to print "Hello, I am {your_name}. Nice to our team name is:"
+
+
+
