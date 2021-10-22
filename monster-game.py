ed10054ed53012124e828a0ebd1c5b36ec7a1891
nouranraouf <nouran.raouf@gmail.com>
2021-10-22 19:12:05 +0200

    Update monster-game.py

--- monster-game.py
+++ monster-game.py
@@ -6,7 +6,8 @@
 ###############################
 
 # YOUR ASSIGNMENT:
-# 1. add below to print "Hello, I am {your_name}. Nice to our team name is:"
+# 1. add below to print "Hello, I am {your_name_1}. our team name is: {xyz}"
+# 2. add below to print "Hello, I am {your_name-2}. our team name is: {xyz}"
 
 
 
